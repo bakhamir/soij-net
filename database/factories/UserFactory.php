0129d3034fe094a3dcb01ae3ca5d4748a3dbbf74
БахытжановА <@206-3>
2024-06-17 21:48:13 +0500

    started doing tests, finished middlewares

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,23 +18,25 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'userName' => fake()->name(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'phoneNum' => 9999999999,
+            'profileId' =>1,
+            'subPlanId' =>1,
+            'sex' => 'm',
+            'age' => 33,
+            'remember_token' => Str::random(10)
+
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      *
+
      * @return static
      */
-    public function unverified()
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+
+
 }
